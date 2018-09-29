e9bf8ec6b5a7b584128bfce26e3b003767bc37a5
Hackathon <volga-hack@yandex.ru>
2018-09-30 02:18:04 +0300

    spring views

--- party-lab-controller/src/main/java/com/rozzer/controller/ControllerPrototype.java
+++ party-lab-controller/src/main/java/com/rozzer/controller/ControllerPrototype.java
@@ -1,42 +1,42 @@
-package com.rozzer.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpSession;
-import java.security.SecureRandom;
-import java.util.Random;
-
-@Deprecated
-@RestController
-public class ControllerPrototype {
-
-    private static final Random RANDOM = new SecureRandom();
-
-    @Value("${api.client_id}")
-    private String clientId;
-
-    @Value("${api.client_secret}")
-    private String clientSecret;
-
-    @RequestMapping(value = "/lab", method = RequestMethod.GET)
-    public String validate(){
-        return "lab successful";
-    }
-
-    @RequestMapping("/")
-    public String index(final HttpSession httpSession) {
-        final long state = RANDOM.nextLong();
-        httpSession.setAttribute("state", state);
-
-        return "<a href=\"https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=user,repo&redirect_uri=http://localhost:8080/logged/authorized&state=" + state + "\">Click</a>";
-    }
-
-    @RequestMapping(value = "/rozzer/lab/lab", method = RequestMethod.POST, produces = "application/json")
-    public String index2(@RequestBody String id){
-        return id;
-    }
-}
+//package com.rozzer.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpSession;
+//import java.security.SecureRandom;
+//import java.util.Random;
+//
+//@Deprecated
+//@RestController
+//public class ControllerPrototype {
+//
+//    private static final Random RANDOM = new SecureRandom();
+//
+//    @Value("${api.client_id}")
+//    private String clientId;
+//
+//    @Value("${api.client_secret}")
+//    private String clientSecret;
+//
+//    @RequestMapping(value = "/lab", method = RequestMethod.GET)
+//    public String validate(){
+//        return "lab successful";
+//    }
+//
+//    @RequestMapping("/")
+//    public String index(final HttpSession httpSession) {
+//        final long state = RANDOM.nextLong();
+//        httpSession.setAttribute("state", state);
+//
+//        return "<a href=\"https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=user,repo&redirect_uri=http://localhost:8080/logged/authorized&state=" + state + "\">Click</a>";
+//    }
+//
+//    @RequestMapping(value = "/rozzer/lab/lab", method = RequestMethod.POST, produces = "application/json")
+//    public String index2(@RequestBody String id){
+//        return id;
+//    }
+//}
