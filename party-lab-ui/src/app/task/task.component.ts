c252da4b202cb3d0683371cbfc8ab7c468d92a75
Hackathon <volga-hack@yandex.ru>
2018-09-30 03:57:04 +0300

    hash strategy

--- party-lab-ui/src/app/task/task.component.ts
+++ party-lab-ui/src/app/task/task.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Task} from './task.model';
-import { ActivatedRoute } from "@angular/router";
-import { TaskService } from "./task.service"
+import {Component, OnInit} from '@angular/core';
+import {Task} from './task.model';
+import {ActivatedRoute} from "@angular/router";
+import {TaskService} from "./task.service"
 
 @Component({
   selector: 'app-task',
@@ -10,21 +10,30 @@ import { TaskService } from "./task.service"
 })
 export class TaskComponent implements OnInit {
 
-   task: Task;
+  task: Task;
+  canCreate: boolean;
 
+  constructor(private route: ActivatedRoute,
+              private taskService: TaskService) {
+  }
 
-    constructor(private route: ActivatedRoute,
-        private taskService: TaskService) {
-        this.task = taskService.tasks[0];
-    }
-
-    ngOnInit() {
-      this.route.params.subscribe(params => {
-        this.taskService.tasks.forEach((p: Task) => {
-          if (p.id == params.id) {
-            this.task = p;
-          }
-        });
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.taskService.tasks.forEach((p: Task) => {
+        if (p.id == params.id) {
+          this.task = p;
+        }
       });
-    }
+      if (this.task === null) {
+        this.task = new Task(params.id, "New Task", "New Task");
+      }
+      if (this.task.id === -1) {
+        this.canCreate = true;
+      }
+    });
+  }
+
+  public createOrUpdate(): void {
+    this.taskService.updateTask(this.task);
+  }
 }
